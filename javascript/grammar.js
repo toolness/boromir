64b9bac30f14cb70869a4acd472ff7789592c41a
Atul Varma <varmaa@gmail.com>
2011-12-26 18:30:48 -0500

    added utils.makePronounDict().

--- javascript/grammar.js
+++ javascript/grammar.js
@@ -1,5 +1,42 @@
 var Grammar = (function() {
+  var PRONOUNS = {
+    male: {
+      himself: "himself",
+      his: "his",
+      him: "him",
+      he: "he"
+    },
+    female: {
+      himself: "herself",
+      his: "her",
+      him: "her",
+      he: "she"      
+    },
+    neuter: {
+      himself: "itself",
+      his: "its",
+      him: "it",
+      he: "it"      
+    },
+    you: {
+      himself: "yourself",
+      his: "your",
+      him: "you",
+      he: "you"      
+    }
+  };
+
   var utils = {
+    makePronounDict: function(gender, prefix) {
+      if (!prefix)
+        prefix = "";
+      if (!(gender in PRONOUNS))
+        throw new Error("invalid gender: " + gender);
+      var dict = {};
+      for (var name in PRONOUNS[gender])
+        dict[prefix + name] = PRONOUNS[gender][name];
+      return dict;
+    },
     applyDict: function(string, dict) {
       return string.replace(/%\(([a-zA-Z0-9_]+)\)s/g, function(str, term) {
         return dict[term].toString();
