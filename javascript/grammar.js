170993f5da8825915228c5228ed744534897ae0f
Atul Varma <varmaa@gmail.com>
2011-12-26 23:08:53 -0500

    added Grammar.View.

--- javascript/grammar.js
+++ javascript/grammar.js
@@ -27,6 +27,11 @@ var Grammar = (function() {
   };
 
   var utils = {
+    extend: function(a, b) {
+      for (var name in b)
+        a[name] = b[name];
+      return a;
+    },
     makePronounDict: function(gender, prefix) {
       if (!prefix)
         prefix = "";
@@ -60,6 +65,49 @@ var Grammar = (function() {
   };
 
   return {
-    utils: utils
+    utils: utils,
+    View: function View(viewer) {
+      var self = {
+        makeGrammarDict: function(subject, object) {
+          function makePosessive(name) {
+            return name + "'s";
+          }
+
+          function fillDict(dict, target, prefix) {
+            if (!prefix)
+              prefix = "";
+
+            if (target === viewer) {
+              utils.extend(dict, utils.makePronounDict("you", prefix));
+              dict[prefix + 'name'] = 'you';
+              dict[prefix + 'name_pos'] = 'your';
+            } else {
+              utils.extend(dict, utils.makePronounDict(target.gender,
+                                                       prefix));
+              dict[prefix + 'name'] = target.name;
+              dict[prefix + 'name_pos'] = makePosessive(target.name);
+            }
+          }
+
+          var dict = {
+            __subject: subject,
+            __object: object
+          };
+
+          fillDict(dict, subject);
+          fillDict(dict, object, "o_");
+          return dict;
+        },
+        parseString: function(string, dict) {
+          var isSubjPlural = (dict.__subject === viewer);
+          var isObjPlural = (dict.__object === viewer);
+          
+          string = utils.parseString(string, isSubjPlural, isObjPlural);
+          return utils.applyDict(string, dict);
+        }
+      };
+
+      return self;
+    }
   };
 })();
